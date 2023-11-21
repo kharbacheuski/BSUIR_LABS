7fdf4246e9c8ee7e4b7ca79a3a01d4babf41ad32
iryl arbacheuski <k.harbacheuski@gmail.com>
2023-11-21 09:33:50 +0300

    lab 6 iiuvm

---
+++ 5_term/ИиУВМ/lab6/Client/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Sockets;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+
+class Program
+{
+    static void Main()
+    {
+
+        // Запрос у пользователя Bluetooth-адреса сервера
+        Console.Write("Введите Bluetooth-адрес сервера (в формате XX:XX:XX:XX:XX:XX): ");
+        string serverAddress = Console.ReadLine();
+
+        BluetoothClient bluetoothClient = new BluetoothClient();
+
+        // Установка соединения
+        var address = BluetoothAddress.Parse(serverAddress);
+        bluetoothClient.Connect(address, BluetoothService.SerialPort);
+
+        Console.WriteLine("Соединение установлено!");
+
+        // Получение потока для приема файла
+        NetworkStream networkStream = bluetoothClient.GetStream();
+
+        // Получение размера файла
+        byte[] fileSizeBytes = new byte[sizeof(long)];
+        networkStream.Read(fileSizeBytes, 0, fileSizeBytes.Length);
+        long fileSize = BitConverter.ToInt64(fileSizeBytes, 0);
+
+        // Получение файла
+        string savePath = "D:\\track.wav";
+        ReceiveFile(networkStream, fileSize, savePath);
+
+        // Закрытие соединения
+        networkStream.Close();
+        bluetoothClient.Close();
+
+        var player = new System.Media.SoundPlayer(savePath);
+        player.Play();
+
+        Console.Read();
+    }
+
+    static void ReceiveFile(Stream stream, long fileSize, string savePath)
+    {
+        using (FileStream fileStream = File.OpenWrite(savePath))
+        {
+            var bytes = new List<byte>();
+
+            byte[] buffer = new byte[1024 * 1024 * 1024];
+            int bytesRead;
+            long bytesReceived = 0;
+
+            while (bytesReceived < fileSize)
+            {
+                bytesRead = stream.Read(buffer, 0, buffer.Length);
+                bytes.AddRange(buffer.Take(bytesRead));
+                bytesReceived += bytesRead;
+            }
+
+            fileStream.Write(bytes.ToArray(), 0, (int)bytesReceived);
+        }
+
+        Console.WriteLine("Файл успешно получен!");
+    }
+}
---
+++ 5_term/ИиУВМ/lab6/Server/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Sockets;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+
+class Program
+{
+    static void Main()
+    {
+        // Запуск сервера Bluetooth
+        BluetoothListener bluetoothListener = new BluetoothListener(BluetoothService.SerialPort);
+        bluetoothListener.Start();
+
+        Console.WriteLine("Ожидание подключения (84:1B:77:17:7E:8C)...");
+
+        // Ожидание подключения клиента
+        BluetoothClient bluetoothClient = bluetoothListener.AcceptBluetoothClient();
+        Console.WriteLine("Успешное подключение!");
+
+        // Получение потока для отправки файла
+        NetworkStream networkStream = bluetoothClient.GetStream();
+
+        // Замените "путь_к_вашему_файлу" на реальный путь к файлу
+        string filePath = "Z:\\music\\Ost.Battlefield2";
+        SendFile(filePath, networkStream);
+
+        // Закрытие соединения
+        networkStream.Close();
+        bluetoothClient.Close();
+        bluetoothListener.Stop();
+    }
+
+    static void SendFile(string filePath, Stream stream)
+    {
+        // Отправка размера файла
+        FileInfo fileInfo = new FileInfo(filePath);
+        byte[] fileSizeBytes = BitConverter.GetBytes(fileInfo.Length);
+        stream.Write(fileSizeBytes, 0, fileSizeBytes.Length);
+
+        // Отправка файла
+        using (FileStream fileStream = File.OpenRead(filePath))
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                stream.Write(buffer, 0, bytesRead);
+            }
+        }
+
+        Console.WriteLine("Файл успешно отправлен!");
+    }
+}
