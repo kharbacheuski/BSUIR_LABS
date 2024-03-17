a86159ba85be86b7adaf8d872455ff8856208a05
KirylHarbacheuski <k.harbacheuski@gmail.com>
2024-03-17 14:01:39 +0300

    bd 4

--- 6_term/ЦОСиИ/DSP/src/charts.js
+++ 6_term/ЦОСиИ/DSP/src/charts.js
@@ -1,4 +1,4 @@
-import { getAxis, FFT, IFFT, complexModule } from "./furie-transform"
+import { getAxis, FFT, IFFT, complexModule, getPoints } from "./furie-transform"
 import Chart from "chart.js/auto";
 import Complex from "complex.js";
 import { fft, ifft,  } from "fft-js";
@@ -6,6 +6,8 @@ import { fft, ifft,  } from "fft-js";
 const N = 64
 const func = (x) => Math.sin(5*x) + Math.cos(x) 
 const axis = getAxis(func, N)
+
+const points = getPoints(N)
 
 new Chart(document.getElementById("initial"), {
     type: 'line',
@@ -22,12 +24,10 @@ new Chart(document.getElementById("initial"), {
 
 const fftRes = FFT(structuredClone(axis.y));
 
-console.log(fftRes)
-
 new Chart(document.getElementById("fftFunction"), {
     type: 'line',
     data: {
-        labels: axis.x,
+        labels: points,
         datasets: [{
             label: 'График функции после БПФ',
             data: fftRes.map(point => complexModule(point)),
@@ -41,7 +41,7 @@ new Chart(document.getElementById("fftFunction"), {
 new Chart(document.getElementById("re-fftFunction"), {
     type: 'line',
     data: {
-        labels: axis.x,
+        labels: points,
         datasets: [{
             label: 'График реальной части после БПФ',
             data: fftRes.map(point => point.re),
@@ -54,7 +54,7 @@ new Chart(document.getElementById("re-fftFunction"), {
 new Chart(document.getElementById("im-fftFunction"), {
     type: 'line',
     data: {
-        labels: axis.x,
+        labels: points,
         datasets: [{
             label: 'График воображаемой части после БПФ',
             data: fftRes.map(point => point.im),
@@ -71,7 +71,7 @@ const data4 = ifftRes.map(point => point.re);
 new Chart(document.getElementById("ifftFunction"), {
     type: 'line',
     data: {
-        labels: axis.x,
+        labels: points,
         datasets: [{
             label: 'График обратной БПФ функции',
             data: data4,
@@ -88,7 +88,7 @@ const modules = data5.map(point => complexModule(new Complex(point[0], point[1])
 new Chart(document.getElementById("libfftFunction"), {
     type: 'line',
     data: {
-        labels: axis.x,
+        labels: points,
         datasets: [{
             label: 'График библиотечной БПФ функции',
             data: modules,
@@ -101,7 +101,7 @@ new Chart(document.getElementById("libfftFunction"), {
 new Chart(document.getElementById("re-libfftFunction"), {
     type: 'line',
     data: {
-        labels: axis.x,
+        labels: points,
         datasets: [{
             label: 'График библиотечной реальной части БПФ функции',
             data: data5.map(point => point[0]),
@@ -114,7 +114,7 @@ new Chart(document.getElementById("re-libfftFunction"), {
 new Chart(document.getElementById("im-libfftFunction"), {
     type: 'line',
     data: {
-        labels: axis.x,
+        labels: points,
         datasets: [{
             label: 'График библиотечной воображаемой части БПФ функции',
             data: data5.map(point => point[1]),
@@ -129,7 +129,7 @@ const data6 = ifft(data5);
 new Chart(document.getElementById("libifftFunction"), {
     type: 'line',
     data: {
-        labels: axis.x,
+        labels: points,
         datasets: [{
             label: 'График библиотечной обратной БПФ функции',
             data: data6.map(point => point[0]),
