90e450b6b5d8bea8c8ef4aeed8dbaf54e06dd3ac
Kirill Harbacheuski <kirill.harbacheuski@biggiko.com>
2024-09-19 20:42:43 +0300

    lab2 cuda, openMP, intrinsics, etc

--- 7_term/АПиТCUDA/matrix-multiply/matrix-multiply.cpp
+++ 7_term/АПиТCUDA/matrix-multiply/matrix-multiply.cpp
@@ -1,69 +1,15 @@
-﻿#include <iostream>
-#include <chrono>
-#include <string>
+﻿#include <immintrin.h>  // Для AVX
+#include "./utils.h"
+#include <omp.h>
 
-class Timer {
-    public:
-        void start()
-        {
-            m_StartTime = std::chrono::system_clock::now();
-            m_bRunning = true;
-        }
+#define MAX 12
 
-        void stop()
-        {
-            m_EndTime = std::chrono::system_clock::now();
-            m_bRunning = false;
-        }
-
-        double elapsedMilliseconds()
-        {
-            std::chrono::time_point<std::chrono::system_clock> endTime;
-
-            if (m_bRunning)
-            {
-                endTime = std::chrono::system_clock::now();
-            }
-            else
-            {
-                endTime = m_EndTime;
-            }
-
-            return std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - m_StartTime).count();
-        }
-
-    private:
-        std::chrono::time_point<std::chrono::system_clock> m_StartTime;
-        std::chrono::time_point<std::chrono::system_clock> m_EndTime;
-        bool                                               m_bRunning = false;
-};
-
-bool isMultiplying(int C1, int R2) {
-    return C1 == R2;
-}
-
-float** createMatrix(int R, int C, bool isEmpty = false) {
-    float** matrix = new float* [R];
-
-    for (int i = 0; i < R; i++) {
-        matrix[i] = new float[C];
-
-        if (!isEmpty) {
-            for (int j = 0; j < C; j++) {
-                matrix[i][j] = (float)(rand() / 10e3);
-            }
-        }
-    }
-
-    return matrix;
-}
-
-float** variant1(float** A, float** B, int R1, int R2, int C1, int C2, bool writeConsole = false) {
+float** non_cache(float** A, float** B, int R1, int R2, int C1, int C2, bool writeConsole = false) {
     auto timer = new Timer();
 
     timer->start();
 
-    float** result = createMatrix(R1, C2, true);
+    float** result = createMatrix<float>(R1, C2, true);
 
     for (int row = 0; row < R1; row++) {
         for (int col = 0; col < C2; col++) {
@@ -89,12 +35,12 @@ float** variant1(float** A, float** B, int R1, int R2, int C1, int C2, bool writ
     return result;
 }
 
-float** variant2(float** A, float** B, int R1, int R2, int C1, int C2, int block_size, bool writeConsole = false) {
+float** with_cache(float** A, float** B, int R1, int R2, int C1, int C2, int block_size, bool writeConsole = false) {
     auto timer2 = new Timer();
 
     timer2->start();
 
-    float** result = createMatrix(R1, C2, true);  // Создаем результирующую матрицу размером R1 x C2 и заполняем её случайными числами
+    float** result = createMatrix<float>(R1, C2, true);  // Создаем результирующую матрицу размером R1 x C2
 
     // Внешний цикл по блокам строк матрицы A
     for (int block_row = 0; block_row < R1; block_row += block_size) {
@@ -140,56 +86,159 @@ float** variant2(float** A, float** B, int R1, int R2, int C1, int C2, int block
     return result;
 }
 
-bool isMatrixCompare(float** A, float** B, int R, int C) {
-    for (int row = 0; row < R; row++) {
-        for (int col = 0; col < C; col++) {
-            if (A[row][col] != B[row][col]) {
-                std::cout << "\nMatrix error: " << A[row][col] << "  " << B[row][col] << "\n\n\n";
 
-                return false;
+double** avx_intrinsics(double** A, double** B, int R1, int R2, int C1, int C2, int block_size, bool writeConsole = false) {
+    auto timer = new Timer();
+
+    timer->start();
+
+    double** result = createMatrix<double>(R1, C2, true);  // Создаем результирующую матрицу размером R1 x C2
+
+    for (int block_row = 0; block_row < R1; block_row += block_size) {
+        for (int inner_block = 0; inner_block < C1; inner_block += block_size) {
+            for (int block_col = 0; block_col < C2; block_col += block_size) {
+                int block_row_end = std::min(block_row + block_size, R1); 
+                int inner_block_end = std::min(inner_block + block_size, C1);
+                int block_col_end = std::min(block_col + block_size, C2); 
+
+                for (int i = block_row; i < block_row_end; i++) {
+                    for (int k = inner_block; k < inner_block_end; k++) {
+                        __m256d a_vec = _mm256_set1_pd(A[i][k]);  // Загружаем элемент из A в каждый элемент регистра (заполняем первый вектор элементами A)
+
+                        // Цикл по столбцам текущего блока матрицы B
+                        for (int j = block_col; j < block_col_end; j += 4) { // AVX способны делать операции сразу с четырьмя последовательно расположенными элементами
+                            // Загружаем четыре элемента из матрицы B
+                            __m256d b_vec = _mm256_loadu_pd(&B[k][j]);
+                            // Загружаем четыре элемента результата
+                            __m256d c_vec = _mm256_loadu_pd(&result[i][j]);
+
+                            // Умножаем и суммируем четыре элемента
+                            __m256d res_vec = _mm256_add_pd(c_vec, _mm256_mul_pd(a_vec, b_vec));
+
+                            // Записываем результат обратно в регистр результата
+                            _mm256_storeu_pd(&result[i][j], res_vec);
+                        }
+                    }
+                }
             }
         }
     }
 
-    return true;
+    timer->stop();
+
+    // Вывод матрицы в консоль
+    if (writeConsole)
+        for (int row = 0; row < R1; ++row) {
+            for (int col = 0; col < C2; ++col) {
+                std::cout << result[row][col] << " ";
+            }
+            std::cout << "\n";
+        }
+
+    std::cout << "\nIntrinsics matrix multiplication time: " << timer->elapsedMilliseconds() << " nanoseconds.\n\n\n";
+
+    return result;
+}
+
+double** avx_intrinsics_openMP(double** A, double** B, int R1, int R2, int C1, int C2, int block_size, bool writeConsole = false) {
+    auto timer = new Timer();
+
+    timer->start();
+
+    double** result = createMatrix<double>(R1, C2, true);  // Создаем результирующую матрицу размером R1 x C2
+
+    // Параллельный блок для обработки блоков строк
+    #pragma omp parallel num_threads(12) 
+    {
+        #pragma omp for // Приводит к тому, что работа в цикле for внутри параллельного региона будет разделена между потоками.
+        for (int block_row = 0; block_row < R1; block_row += block_size) {
+            for (int inner_block = 0; inner_block < C1; inner_block += block_size) {
+                for (int block_col = 0; block_col < C2; block_col += block_size) {
+                    int block_row_end = std::min(block_row + block_size, R1);
+                    int inner_block_end = std::min(inner_block + block_size, C1);
+                    int block_col_end = std::min(block_col + block_size, C2);
+                    for (int i = block_row; i < block_row_end; i++) {
+                        for (int k = inner_block; k < inner_block_end; k++) {
+                            __m256d a_vec = _mm256_set1_pd(A[i][k]);  // Загружаем элемент из A в каждый элемент регистра
+
+                            // Цикл по столбцам текущего блока матрицы B
+                            for (int j = block_col; j < block_col_end; j += 4) {
+                                // Загружаем четыре элемента из матрицы B
+                                __m256d b_vec = _mm256_loadu_pd(&B[k][j]);
+                                // Загружаем четыре элемента результата
+                                __m256d c_vec = _mm256_loadu_pd(&result[i][j]);
+
+                                // Умножаем и суммируем четыре элемента
+                                __m256d res_vec = _mm256_add_pd(c_vec, _mm256_mul_pd(a_vec, b_vec));
+
+                                // Записываем результат обратно в регистр результата
+                                _mm256_storeu_pd(&result[i][j], res_vec);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    timer->stop();
+
+    // Вывод матрицы в консоль
+    if (writeConsole) {
+        for (int row = 0; row < R1; ++row) {
+            for (int col = 0; col < C2; ++col) {
+                std::cout << result[row][col] << " ";
+            }
+            std::cout << "\n";
+        }
+    }
+
+    std::cout << "\nOpenMP Intrinsics matrix multiplication time: " << timer->elapsedMilliseconds() << " nanoseconds.\n\n\n";
+
+    return result;
 }
 
 int getBlockSize(int cache_level) {
     return (int)(cache_level * (0.9 / 3));
 }
 
-void MultiplyWithOutAMP() {
+int main() {
     srand(time(0));
 
-    const int R1 = 1000;
+    const int R1 = 1500;
     const int C1 = 1500;
 
     const int R2 = 1500;
-    const int C2 = 800;
+    const int C2 = 600;
 
     int L1 = 32; // 32 ячейки - тоесть 32*32*4 байта - 4КБ.  L1 кеш - 64 КБ
     int L2 = 256; // 256 ячеек - тоесть 256*256*4 байта - 256КБ. L2 кеш - 512 КБ
     int L3 = 1024; // 1024 ячеек - тоесть 1024*1024*4 байта - 4МБ.  L3 кеш - 16МБ
     int OUT_OF_CASH = 4096; // 4096 ячеек - тоесть 4096*4096*4 байта - 67МБ
 
-    float** A = createMatrix(R1, C1);
-    float** B = createMatrix(R2, C2);
+    float** A = createMatrix<float>(R1, C1);
+    float** B = createMatrix<float>(R2, C2);
 
     bool isConsoleOutput = false;
 
     if (isMultiplying(C1, R2)) {
-        float** one = variant1(A, B, R1, R2, C1, C2, isConsoleOutput);
-        float** two = variant2(A, B, R1, R2, C1, C2, L1, isConsoleOutput);
+        float** one = non_cache(A, B, R1, R2, C1, C2, isConsoleOutput);
 
-        bool equal = isMatrixCompare(one, two, R1, C2);
+        float** two = with_cache(A, B, R1, R2, C1, C2, L1, isConsoleOutput);
 
-        std::cout << "\nIs matrix equal: " << equal << std::endl;
+        double** Acopy = convertFloatMatrixToDouble(A, R1, C1);
+        double** Bcopy = convertFloatMatrixToDouble(B, R2, C2);
+
+        double** three = avx_intrinsics(Acopy, Bcopy, R1, R2, C1, C2, L1, isConsoleOutput);
+
+        double** four = avx_intrinsics_openMP(Acopy, Bcopy, R1, R2, C1, C2, L1, isConsoleOutput);
+
+        //float** threeCopy = convertDoubleMatrixToFloat(three, R1, C2);
+
+        //bool equal = isMatrixCompare(one, threeCopy, R1, C2);
+        //std::cout << "\nIs matrix equal: " << equal << std::endl;
     }
     else {
         std::cout << "\nMatrix CAN'T be multiplying" << std::endl;
     }
-}
-
-int main() {
-    MultiplyWithOutAMP();
 }
---
+++ 7_term/АПиТCUDA/matrix-multiply/utils.h
@@ -0,0 +1,107 @@
+#pragma once
+#include <iostream>
+#include <chrono>
+#include <string>
+
+class Timer {
+public:
+    void start()
+    {
+        m_StartTime = std::chrono::system_clock::now();
+        m_bRunning = true;
+    }
+
+    void stop()
+    {
+        m_EndTime = std::chrono::system_clock::now();
+        m_bRunning = false;
+    }
+
+    double elapsedMilliseconds()
+    {
+        std::chrono::time_point<std::chrono::system_clock> endTime;
+
+        if (m_bRunning)
+        {
+            endTime = std::chrono::system_clock::now();
+        }
+        else
+        {
+            endTime = m_EndTime;
+        }
+
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - m_StartTime).count();
+    }
+
+private:
+    std::chrono::time_point<std::chrono::system_clock> m_StartTime;
+    std::chrono::time_point<std::chrono::system_clock> m_EndTime;
+    bool                                               m_bRunning = false;
+};
+
+bool isMultiplying(int C1, int R2) {
+    return C1 == R2;
+}
+
+double** convertFloatMatrixToDouble(float** floatMatrix, int R, int C) {
+    // Создаем новую матрицу типа float**
+    double** doubleMatrix = new double* [R];
+
+    for (int i = 0; i < R; i++) {
+        doubleMatrix[i] = new double[C];
+
+        for (int j = 0; j < C; j++) {
+            // Преобразуем каждый элемент из double в float
+            doubleMatrix[i][j] = static_cast<float>(floatMatrix[i][j]);
+        }
+    }
+
+    return doubleMatrix;
+}
+
+float** convertDoubleMatrixToFloat(double** doubleMatrix, int R, int C) {
+    // Создаем новую матрицу типа float**
+    float** floatMatrix = new float* [R];
+
+    for (int i = 0; i < R; i++) {
+        floatMatrix[i] = new float[C];
+
+        for (int j = 0; j < C; j++) {
+            // Преобразуем каждый элемент из double в float
+            floatMatrix[i][j] = static_cast<float>(doubleMatrix[i][j]);
+        }
+    }
+
+    return floatMatrix;
+}
+
+template <typename T>
+T** createMatrix(int R, int C, bool isEmpty = false) {
+    T** matrix = new T * [R];
+
+    for (int i = 0; i < R; i++) {
+        matrix[i] = new T[C];
+
+        if (!isEmpty) {
+            for (int j = 0; j < C; j++) {
+                matrix[i][j] = static_cast<T>(rand() / 10e3);
+            }
+        }
+    }
+
+    return matrix;
+}
+
+bool isMatrixCompare(float** A, float** B, int R, int C) {
+    for (int row = 0; row < R; row++) {
+        for (int col = 0; col < C; col++) {
+            if (A[row][col] != B[row][col]) {
+                std::cout << "\nMatrix error: " << A[row][col] << "  " << B[row][col] << "\n\n\n";
+
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
